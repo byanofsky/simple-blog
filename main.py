7cdd9ddd57ca8b327a7c17a4e03fb47c0e8798f4
byanofsky <byanofsky@me.com>
2016-12-30 17:21:30 -0800

    add notes

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ class FrontPageHandler(Handler):
     def get(self):
         # posts = Post.query().order(-Post.created).fetch_page(10)
         # TODO: multi page with fetch_page
-        # base_url = self.get_url('frontpage')
         posts = Post.get_all()
+        # TODO: can I fix up url handling some more?
         self.render('frontpage.html', posts=posts, url_handler=self.get_url)
 
 class SignUpHandler(Handler):
