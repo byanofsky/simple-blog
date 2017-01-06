602363841836c4f565add6394fa569e1ff6ade89
byanofsky <byanofsky@me.com>
2017-01-05 17:04:47 -0800

    Clean up auth file

--- auth.py
+++ auth.py
@@ -7,8 +7,17 @@ with open("config.yml", 'r') as ymlfile:
 
 # TODO: need to organize auth and validate
 
-# TODO: get secret key from config file
+# get secret key from config file
 SECRET_KEY = cfg['secret_key']
+
+def make_hashed_pw(pw):
+    return pbkdf2_sha256.hash(pw)
+
+def verify_pw(pw, hash):
+    return pbkdf2_sha256.verify(pw, hash)
+
+def valid_login(u, pw):
+    return verify_pw(pw, u.hashed_pw)
 
 def hash_str(s):
     return hmac.new(SECRET_KEY, s).hexdigest()
@@ -21,14 +30,7 @@ def check_secure_val(h):
     if h == make_secure_val(val):
         return val
 
-def make_hashed_pw(pw):
-    return pbkdf2_sha256.hash(pw)
-
-def verify_pw(pw, hash):
-    return pbkdf2_sha256.verify(pw, hash)
-
-def valid_login(u, pw):
-    return verify_pw(pw, u.hashed_pw)
+# TODO: perhaps move these to a file for logging in and out
 
 def get_user_cookie(handler):
     return handler.request.cookies.get('user_id')
@@ -37,8 +39,6 @@ def get_user_cookie_id(handler):
     u_cookie = get_user_cookie(handler)
     if u_cookie:
         return check_secure_val(u_cookie)
-    else:
-        return None
 
 def set_user_cookie(handler, u_key):
     u_cookie = make_secure_val(str(u_key.id()))
