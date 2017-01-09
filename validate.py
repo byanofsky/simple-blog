f7ee067c0253c467718c2d69c0ec789135a32bbc
byanofsky <byanofsky@me.com>
2017-01-09 12:30:57 -0800

    Fix validate.py to pep style guide

--- validate.py
+++ validate.py
@@ -1,21 +1,25 @@
 import re
+
 from auth import valid_login
 
 PASSWORD_RE = re.compile(r'^.{3,20}$')
 EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 
+
 def valid_email(email):
     return EMAIL_RE.match(email)
+
 
 def valid_password(pw):
     return PASSWORD_RE.match(pw)
 
+
 def password_match(pw, verify):
     return pw == verify
 
+
 def signup_errors(email, pw, verify, user_exists):
     errors = {}
-
     if user_exists:
         errors['user_exists'] = ('There is already a user registered ' +
                                  'with this email.')
@@ -26,8 +30,8 @@ def signup_errors(email, pw, verify, user_exists):
     if not password_match(pw, verify):
         errors['verify'] = ('Your passwords do not match. Please re-enter ' +
                             'your passwords.')
-
     return errors
+
 
 def login_errors(email, u, pw):
     if not valid_email(email):
@@ -37,39 +41,35 @@ def login_errors(email, u, pw):
     elif not valid_login(u, pw):
         return 'The password is incorrect. Please try again.'
 
+
 def newpost_errors(title, body):
     errors = {}
-
     if not title:
         errors['title'] = 'Please enter a title.'
     if not body:
-        errors['body'] = ('Your blog post cannot be blank. Please enter your ' +
-                          'blog post content.')
-
+        errors['body'] = ('Your blog post cannot be blank. ' +
+                          'Please enter your blog post content.')
     return errors
+
 
 def comment_errors(comment):
     errors = {}
-
     if not comment:
         errors['comment'] = 'Comment cannot be blank.'
-
     return errors
+
 
 def editpost_errors(title, body):
     errors = {}
-
     if not title:
         errors['title'] = 'Title cannot be blank.'
     if not body:
         errors['body'] = 'Your blog post cannot be blank.'
-
     return errors
+
 
 def editcomment_errors(body):
     errors = {}
-
     if not body:
         errors['body'] = 'Your comment cannot be blank.'
-
     return errors
