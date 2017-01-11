c176bbcc2306be3c2eb195ea7362fcb3eb35a09b
byanofsky <byanofsky@me.com>
2017-01-10 17:41:00 -0800

    Adjust loginhandler to account for new structure

--- handlers/loginhandler.py
+++ handlers/loginhandler.py
@@ -1,30 +1,31 @@
 from handlers.basehandler import BaseHandler
+from models.user import User
+import validate
+import auth
+# TODO: can we get rid of so many imports? User, auth, validate
 
 
 class LoginHandler(BaseHandler):
-    page_title = 'Login'
-
     def get(self):
-        self.render('login.html', signup=self.uri_for('signup'))
+        self.render('login.html')
 
     def post(self):
         # Get Login POST data
         email = self.request.get('email')
         pw = self.request.get('password')
-        u = User.get_by_email(email)
+        user = User.get_by_email(email)
 
         # TODO: Can this be moved into one function?
         # TODO: Could return errors, user, in one function
         # u, error = check_login()
 
         # Check for login form errors
-        errors = validate.login_errors(email, u, pw)
+        errors = validate.login_errors(user, email, pw)
 
         if errors:
             # If form errors, display form with errors
-            self.render('login.html', email=email, errors=errors,
-                        signup=self.uri_for('signup'))
+            self.render('login.html', email=email, errors=errors)
         else:
             # If form validates, set user cookie and direct to welcome page
-            auth.set_user_cookie(self, u.key)
+            auth.set_user_cookie(self, user.key)
             self.redirect_to('welcome')
