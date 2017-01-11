cc7852b6498f6a3cfe371cf98c056ff0d1df9a0b
byanofsky <byanofsky@me.com>
2017-01-11 11:35:41 -0800

    Set up newposthandler

--- handlers/newposthandler.py
+++ handlers/newposthandler.py
@@ -1,29 +1,29 @@
 from handlers.basehandler import BaseHandler
+from validation import require_user
+import validate
+from models.post import Post
 
 
 class NewPostHandler(BaseHandler):
-    page_title = 'New Post'
+    @require_user
+    def get(self, user):
+        self.render('newpost.html')
 
-    def get(self):
-        if self.u:
-            self.render('newpost.html')
+    @require_user
+    def post(self, user):
+        title = self.request.get('title')
+        body = self.request.get('body')
+        # Validate newpost form
+        errors = validate.newpost_errors(title, body)
+
+        if errors:
+            self.render(
+                'newpost.html',
+                title=title,
+                body=body,
+                errors=errors
+            )
         else:
-            self.error_redirect('createpost')
-
-    def post(self):
-        if self.u:
-            title = self.request.get('title')
-            body = self.request.get('body')
-            # Validate newpost form
-            errors = validate.newpost_errors(title, body)
-
-            if errors:
-                self.render('newpost.html', title=title, body=body,
-                            errors=errors)
-            else:
-                # Create post in database, and redirect to singlepost
-                p_key = Post.create(title, body, self.u)
-                self.redirect_to('singlepost', post_id=p_key.id())
-        else:
-            # If no user logged in, redirect to error page
-            self.error_redirect('createpost')
+            # Create post in database, and redirect to singlepost
+            post_key = Post.create(title, body, user)
+            self.redirect_to('viewpost', post_id=post_key.id())
