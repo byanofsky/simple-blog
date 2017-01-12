9eaa63690df35a72158bfa683a7264677d5820e7
byanofsky <byanofsky@me.com>
2017-01-11 18:54:38 -0800

    Add note

--- handlers/unlikeposthandler.py
+++ handlers/unlikeposthandler.py
@@ -4,6 +4,7 @@ from validation import user_can_unlike_post
 
 class UnlikePostHandler(BaseHandler):
     # TODO: do I need a get?
+    # Currently gives a 405 error
     # def get(self, post_id):
     #     print 'get liked'
 
