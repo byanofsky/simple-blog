4a0f720ffd27d1a154833586b7b20e2f750b8527
byanofsky <byanofsky@me.com>
2017-01-12 14:00:29 -0800

    Add secret key to config file

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # specified e.g.
 #   Third party api keys/secrets
 app_config = {
-    'API_KEY': 'SOME API KEY.....',
+    'secret_key': 'SOME API KEY.....',
     'debug': True,
     # TODO: use in basehandler. Is there a better way?
     'root_dir': os.path.dirname(os.path.abspath(__file__))
