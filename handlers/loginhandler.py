28a6c3c892139cc2a213ebcda54f3eb6efb1ede5
byanofsky <byanofsky@me.com>
2017-01-13 09:01:43 -0800

    Fix up login handling

--- handlers/loginhandler.py
+++ handlers/loginhandler.py
@@ -1,5 +1,6 @@
 from handlers.basehandler import BaseHandler
 import modules.user_auth as user_auth
+import modules.form_validation as form_validation
 # TODO: can we get rid of so many imports? User, auth, validate
 
 
@@ -13,10 +14,19 @@ class LoginHandler(BaseHandler):
         pw = self.request.get('password')
 
         # Check for login form errors
-        errors, user = user_auth.validate_login(email, pw)
-        if errors:
-            self.render('login.html', email=email, errors=errors)
+        # errors, user = user_auth.validate_login(email, pw)
+        form_errors = form_validation.check_login(email)
+        if form_errors:
+            self.render('login.html', email=email, form_errors=form_errors)
         else:
-            # If form validates, set user cookie and direct to welcome page
-            self.set_secure_cookie('user_id', user.key.id())
-            self.redirect_to('welcome')
+            user, login_errors = user_auth.validate_login(email, pw)
+            if login_errors:
+                self.render(
+                    'login.html',
+                    email=email,
+                    login_errors=login_errors
+                )
+            else:
+                # If form validates, set user cookie and direct to welcome page
+                self.set_secure_cookie('user_id', user.key.id())
+                self.redirect_to('welcome')
--- modules/user_auth.py
+++ modules/user_auth.py
@@ -4,18 +4,14 @@ from models.user import User
 from modules.form_validation import valid_email
 
 
-# Check login. Returns errors (if any) and user object (if user exists)
 def validate_login(email, pw):
-    errors = {}
-    user = None
-    if not valid_email(email):
-        errors['email'] = True
-    else:
-        user = User.get_by_email(email)
-        if not user:
-            errors['user'] = True
-        else:
-            valid_login = secure.verify_pw(pw, user.hashed_pw)
-            if not valid_login:
-                errors['valid_login'] = True
-    return errors, user
+    user = User.get_by_email(email)
+    login_errors = {}
+    if not user:
+        login_errors['user'] = True
+    elif not secure.verify_pw(pw, user.hashed_pw):
+        login_errors['wrong_pw'] = True
+    return user, login_errors
+
+def user_login(email, pw):
+    pass
