cac12a7c691d4b1483c75067953d6f19209c86b4
byanofsky <byanofsky@me.com>
2017-01-13 11:08:39 -0800

    Use require_user rather than _or_redirect

--- handlers/welcomehandler.py
+++ handlers/welcomehandler.py
@@ -1,8 +1,8 @@
 from handlers.basehandler import BaseHandler
-from modules.validation import require_user_or_redirect
+from modules.validation import require_user
 
 
 class WelcomeHandler(BaseHandler):
-    @require_user_or_redirect('login')
+    @require_user('login')
     def get(self, user):
         self.render('welcome.html', user=user)
