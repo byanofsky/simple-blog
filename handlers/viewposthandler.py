4b14362b2ef86040750884f2562820ee5d15fda9
byanofsky <byanofsky@me.com>
2017-01-13 16:12:34 -0800

    Fix viewpost to recent changes in comments

--- handlers/viewposthandler.py
+++ handlers/viewposthandler.py
@@ -1,31 +1,15 @@
 from handlers.basehandler import BaseHandler
-from modules.validation import get_user, require_user, post_exists
+from modules.validation import get_user, post_exists
 
 
 class ViewPostHandler(BaseHandler):
     @post_exists
     @get_user
-    def get(self, user, post_id, post):
+    def get(self, user, post, post_id):
         self.render(
             'viewpost.html',
             user=user,
             post=post,
+            post_id=post_id,
             comments=post.get_comments()
         )
-
-    # @post_exists
-    # @require_user
-    # def post(self, user, post_id, post):
-        # Get query string data for "action"
-        # action = self.request.get('action')
-        # elif action == 'like':
-        #     self.write('like')
-        # elif action == 'unlike':
-        #     self.write('unlike')
-            # If action isn't comment, it must be like or unlike.
-            # Uses "else" instead of "elif" so redirect can be used.
-            # if action == 'like':
-            #     self.u.like(self.p)
-            # elif action == 'unlike':
-            #     self.u.unlike(self.p)
-        # self.redirect_to('singlepost', post_id=post_id)
