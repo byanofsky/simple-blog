8f255b30b4cfd5c4a75f151bfe66e1a48d261392
byanofsky <byanofsky@me.com>
2017-01-13 16:19:58 -0800

    Restructure deletecommenthandler

--- handlers/deletecommenthandler.py
+++ handlers/deletecommenthandler.py
@@ -4,11 +4,11 @@ from modules.validation import user_owns_comment
 
 class DeleteCommentHandler(BaseHandler):
     @user_owns_comment
-    def get(self, user, comment):
+    def get(self, user, comment, comment_key):
         self.render('deletecomment.html', comment=comment)
 
     @user_owns_comment
-    def post(self, user, comment):
+    def post(self, user, comment, comment_key):
         parent_post_id = comment.key.parent().id()
         comment.delete()
         self.render(
