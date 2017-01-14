fed056374bc95efbe1b6605a28e8f13709561656
byanofsky <byanofsky@me.com>
2017-01-13 18:41:17 -0800

    Refactor deletepost

--- handlers/deleteposthandler.py
+++ handlers/deleteposthandler.py
@@ -4,12 +4,12 @@ from modules.validation import user_owns_post
 
 class DeletePostHandler(BaseHandler):
     @user_owns_post
-    def get(self, user, post_id, post):
+    def get(self, user, post, post_id):
         # make sure user wants to delete. Confirm or cancel
         self.render('deletepost.html', post=post)
 
     @user_owns_post
-    def post(self, user, post_id, post):
+    def post(self, user, post, post_id):
         # if confirm, post here
         post.delete()
         self.render('deletepost.html', deleted=True)
